c1630f7b0e80a86dfa697dd4cf0bb198aa6ac681
mikefallon <mikefallon81@gmail.com>
2020-04-28 09:55:00 +0100

    Link imported

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 const PageOne = () => {
-    return <div>Page One</div>
+    return <div>Page One
+    
+            <Link to="/pageTwo">Navigate to page two</Link>
+    </div>
 };
 
 const PageTwo = () => {
     return <div>Page Two
         <button>Click Me!</button>
-    </div>
+            <Link to="/">Navigate to Page one</Link>
+        </div>
 };
 
 
 const App = () => {
     return <div>
-        <BrowserRouter>
-            <div>
-                <Route path="/" exact component={PageOne} />
-                <Route path="/pagetwo" component={PageTwo} />
-            </div>
-        </BrowserRouter>
-    </div>;
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={PageOne} />
+                    <Route path="/pagetwo" component={PageTwo} />
+                </div>
+            </BrowserRouter>
+        </div>;
 };
 
 export default App;
